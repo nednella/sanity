8068bfdb0bbf11939543884ae02189b58ed3e047
nednella <19449811+nednella@users.noreply.github.com>
2025-11-22 20:48:54 +0000

    Fix main.css to match use of data attributes

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -5,5 +5,5 @@ export const Route = createFileRoute("/")({
 })
 
 function RouteComponent() {
-    return <h1 className="text-3xl font-bold text-blue-500 underline">Hello world!</h1>
+    return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
