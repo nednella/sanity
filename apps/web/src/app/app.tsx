da0b9be05c7c2445b0b4e03b615fbf2d261c35ad
nednella <19449811+nednella@users.noreply.github.com>
2025-11-22 18:34:41 +0000

    feat(web): set up prettier

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,10 +1,5 @@
 function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline text-blue-500">
-      Hello world!
-    </h1>
-  )
+    return <h1 className="text-3xl font-bold text-blue-500 underline">Hello world!</h1>
 }
 
 export default App
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './main.css'
-import App from './app/app'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from "./app/app"
+import "./main.css"
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
