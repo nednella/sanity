d775e8f095dd550e649e8dc37fcf4c26877730c7
nednella <19449811+nednella@users.noreply.github.com>
2025-11-26 19:35:55 +0000

    Update background gradient styling

--- apps/web/src/components/landing/background-gradient.tsx
+++ apps/web/src/components/landing/background-gradient.tsx
@@ -1,3 +1,3 @@
 export function BackgroundGradient() {
-    return <div className="fixed inset-0 z-[-1] bg-linear-to-t from-neutral-950 to-cyan-900" />
+    return <div className="fixed inset-0 z-[-1] bg-linear-to-t from-neutral-950 to-cyan-800" />
 }
