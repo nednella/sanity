313f2b528f65193582eb3cb93f171b7583c004f6
nednella <19449811+nednella@users.noreply.github.com>
2025-11-26 22:02:18 +0000

    Move index page components to landing/index/

--- apps/web/src/routes/(public)/index.tsx
+++ apps/web/src/routes/(public)/index.tsx
@@ -1,11 +1,11 @@
 import { useUser } from "@clerk/clerk-react"
 import { createFileRoute } from "@tanstack/react-router"
 
-import { BackgroundGradient } from "@/components/landing/background-gradient"
-import { Cta } from "@/components/landing/cta"
-import { Hero } from "@/components/landing/hero"
-import { Highlights } from "@/components/landing/highlights"
-import { Stats } from "@/components/landing/stats"
+import { BackgroundGradient } from "@/components/landing/index/background-gradient"
+import { Cta } from "@/components/landing/index/cta"
+import { Hero } from "@/components/landing/index/hero"
+import { Highlights } from "@/components/landing/index/highlights"
+import { Stats } from "@/components/landing/index/stats"
 
 export const Route = createFileRoute("/(public)/")({
     component: IndexPage
