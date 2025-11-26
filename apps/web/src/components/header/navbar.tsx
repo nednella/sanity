eaedbe078a5d5787a8eb76b508760a56d6435f8d
nednella <19449811+nednella@users.noreply.github.com>
2025-11-26 05:44:12 +0000

    Update navbar styling

--- apps/web/src/components/header/navbar.tsx
+++ apps/web/src/components/header/navbar.tsx
@@ -10,8 +10,8 @@ import { options } from "./navbar.config"
 
 export function Navbar() {
     return (
-        <header className="bg-background/90 fixed z-50 w-full backdrop-blur-md">
-            <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-6 py-2 md:rounded-md">
+        <header className="bg-background/80 fixed z-50 w-full backdrop-blur-sm">
+            <div className="mx-auto flex h-24 max-w-7xl items-center justify-between px-6 py-2 md:rounded-md">
                 {/* mobile menu */}
                 <Menu
                     className="md:hidden"
