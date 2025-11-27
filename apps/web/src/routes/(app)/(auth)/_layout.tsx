5a19adacc073e3b4815d79b0953619b2c6288bf4
nednella <19449811+nednella@users.noreply.github.com>
2025-11-27 21:14:06 +0000

    Open auth dialog when blocked from accessing authenticated content

--- apps/web/src/routes/(app)/(auth)/_layout.tsx
+++ apps/web/src/routes/(app)/(auth)/_layout.tsx
@@ -1,10 +1,13 @@
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router"
 
+import { useAuthDialog } from "@/stores/auth-dialog.store"
 import { isAuthenticated } from "@/utils/auth"
 
 export const Route = createFileRoute("/(app)/(auth)")({
     beforeLoad({ context: { auth } }) {
         if (!isAuthenticated(auth)) {
+            useAuthDialog.getState().onOpen()
+
             throw redirect({
                 to: "/",
                 replace: true
