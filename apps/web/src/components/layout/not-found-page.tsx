56c846f5574e083f56009c2ac0958f456b3dfa9f
nednella <119449811+nednella@users.noreply.github.com>
2025-11-27 22:24:41 +0000

    feat(web): add 404 page (#13)

---
+++ apps/web/src/components/layout/not-found-page.tsx
@@ -0,0 +1,28 @@
+import { Link } from "@tanstack/react-router"
+
+import { Button } from "@/lib/ui/components/button"
+import { H1 } from "@/lib/ui/components/typography/h1"
+import { Lead } from "@/lib/ui/components/typography/lead"
+
+export function NotFound() {
+    // TODO: add logo
+    return (
+        <section className="bg-background flex min-h-screen items-center justify-center">
+            <div className="container mx-auto">
+                <div className="text-center">
+                    <H1 className="mb-6">Page not found</H1>
+                    <Lead className="mb-6 text-lg">we can't seem to find the page you are looking for</Lead>
+                    <div className="flex flex-col items-center justify-center gap-4 sm:flex-row">
+                        <Button
+                            asChild
+                            size="lg"
+                            className="w-64 rounded-full px-8 sm:w-fit"
+                        >
+                            <Link to="/">Go back home</Link>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
