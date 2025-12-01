8609e2f45b006e1102491cbd6a03be0df4b98a39
nednella <119449811+nednella@users.noreply.github.com>
2025-12-01 02:20:19 +0000

    feat(web): update landing page stats with accurate numbers (#17)

--- apps/web/src/components/landing/index/stats.tsx
+++ apps/web/src/components/landing/index/stats.tsx
@@ -13,10 +13,10 @@ type Stat = {
 }
 
 const stats: Stat[] = [
-    { icon: Users, label: "Active Members", value: "250+" },
+    { icon: Users, label: "Active Members", value: "180+" },
     { icon: Timer, label: "World Record Times", value: "15+" },
     { icon: Gauge, label: "Total Clan EHB", value: "550k+" },
-    { icon: HandCoins, label: "Total Loot Value", value: "1000b+" }
+    { icon: HandCoins, label: "Total Loot Value", value: "1.4t+" }
 ]
 
 export function Stats() {
