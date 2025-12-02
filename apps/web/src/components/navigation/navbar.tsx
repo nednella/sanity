9101093ad886252366e5c1bd9cafe6760d786f43
nednella <19449811+nednella@users.noreply.github.com>
2025-12-02 19:12:33 +0000

    Fix navbar logo link width

--- apps/web/src/components/navigation/navbar.tsx
+++ apps/web/src/components/navigation/navbar.tsx
@@ -22,7 +22,7 @@ export function Navbar() {
                 >
                     <Link
                         to="/"
-                        className="font-bold"
+                        className="w-fit font-bold"
                     >
                         Sanity
                     </Link>
