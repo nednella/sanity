d0fb9f399e8812079822992b4201a975f0b87fb3
nednella <19449811+nednella@users.noreply.github.com>
2025-12-02 18:23:32 +0000

    Add hoverable desktop navigation to navbar

---
+++ apps/web/src/components/navigation/navbar-desktop-navigation.tsx
@@ -0,0 +1,116 @@
+import { Link, linkOptions } from "@tanstack/react-router"
+
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger
+} from "@/lib/ui/components/navigation-menu"
+import { Small } from "@/lib/ui/components/typography/small"
+import { useIsMobile } from "@/lib/ui/hooks/use-mobile"
+
+const aboutLinks = linkOptions([
+    {
+        title: "Background",
+        to: "/about",
+        description: "Learn more about us"
+    },
+    {
+        title: "Requirements",
+        to: "/about",
+        description: "Find out what it takes to join"
+    },
+    {
+        title: "Ranking System",
+        to: "/about",
+        description: "How we progress our members"
+    }
+])
+
+const memberLinks = linkOptions([
+    {
+        title: "Placeholder",
+        to: "/about",
+        description: "Placeholder"
+    },
+    {
+        title: "Placeholder",
+        to: "/about",
+        description: "Placeholder"
+    },
+    {
+        title: "Placeholder",
+        to: "/about",
+        description: "Placeholder"
+    }
+])
+
+export function DesktopNavigation() {
+    const isMobile = useIsMobile()
+
+    return (
+        <NavigationMenu
+            viewport={isMobile}
+            className="hidden lg:flex"
+        >
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>About</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="flex w-fit min-w-50 flex-col gap-1">
+                            {aboutLinks.map((link) => (
+                                <ListItem
+                                    key={link.title}
+                                    to={link.to}
+                                    title={link.title}
+                                    description={link.description}
+                                />
+                            ))}
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>Members</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="flex w-fit min-w-50 flex-col gap-1">
+                            {memberLinks.map((link) => (
+                                <ListItem
+                                    key={link.title}
+                                    to={link.to}
+                                    title={link.title}
+                                    description={link.description}
+                                />
+                            ))}
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild>
+                        <Link to="/authenticated">Authenticated</Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
+
+function ListItem({
+    title,
+    description,
+    to,
+    ...props
+}: React.ComponentPropsWithoutRef<"li"> & { to: string; description: string }) {
+    return (
+        <li {...props}>
+            <NavigationMenuLink asChild>
+                <Link to={to}>
+                    <Small>{title}</Small>
+                    <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">{description}</p>
+                </Link>
+            </NavigationMenuLink>
+        </li>
+    )
+}
--- apps/web/src/components/navigation/navbar.tsx
+++ apps/web/src/components/navigation/navbar.tsx
@@ -1,19 +1,15 @@
-import { SignedIn, SignedOut } from "@clerk/clerk-react"
 import { Link } from "@tanstack/react-router"
-import { Menu } from "lucide-react"
 
-import { Button } from "@/lib/ui/components/button"
-
-import { AccountButton } from "../auth/account-button"
-import { LoginButton } from "../auth/login-button"
-import { ThemeToggle } from "../theme/theme-toggle"
-import { options } from "./navbar.config"
+import { DesktopButtons } from "./navbar-desktop-buttons"
+import { DesktopNavigation } from "./navbar-desktop-navigation"
+import { MobileButtons } from "./navbar-mobile-buttons"
 
 /**
  * TODO
  *
- * 1. Add desktop hover links
  * 2. Add mobile/tablet sidebar navigation with nested links to replace hover links
+ * 3. Remove this comment block
+ * 4. merge feature as mobile navigation
  */
 
 export function Navbar() {
@@ -24,49 +20,15 @@ export function Navbar() {
                     className="bg-background flex h-16 items-center justify-between px-6 shadow-md lg:grid
                         lg:grid-cols-[1fr_auto_1fr] lg:rounded-2xl lg:border"
                 >
-                    {/* mobile menu toggle */}
-                    <Menu
-                        className="md:hidden"
-                        onClick={() => {}}
-                    />
-
-                    {/* logo */}
                     <Link
                         to="/"
                         className="font-bold"
                     >
                         Sanity
                     </Link>
-
-                    {/* nav links */}
-                    <nav className="hidden lg:flex lg:gap-8">
-                        {options.map((link) => (
-                            <Button
-                                key={link.label}
-                                asChild
-                                variant="ghost"
-                            >
-                                <Link
-                                    to={link.to}
-                                    activeProps={{ className: "text-foreground" }}
-                                    inactiveProps={{ className: "text-muted-foreground" }}
-                                >
-                                    {link.label}
-                                </Link>
-                            </Button>
-                        ))}
-                    </nav>
-
-                    {/* buttons */}
-                    <div className="flex items-center justify-end gap-4">
-                        <ThemeToggle />
-                        <SignedOut>
-                            <LoginButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <AccountButton />
-                        </SignedIn>
-                    </div>
+                    <DesktopNavigation />
+                    <DesktopButtons />
+                    <MobileButtons />
                 </div>
             </div>
         </header>
