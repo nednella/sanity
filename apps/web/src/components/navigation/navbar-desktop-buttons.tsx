f1f16d27299efc836d0833b9143b998fb298a884
nednella <19449811+nednella@users.noreply.github.com>
2025-12-02 19:05:34 +0000

    Add AccountButton variant prop with sidebar version

--- apps/web/src/components/navigation/navbar-desktop-buttons.tsx
+++ apps/web/src/components/navigation/navbar-desktop-buttons.tsx
@@ -12,7 +12,7 @@ export function DesktopButtons() {
                 <LoginButton />
             </SignedOut>
             <SignedIn>
-                <AccountButton />
+                <AccountButton variant="avatar" />
             </SignedIn>
         </div>
     )
