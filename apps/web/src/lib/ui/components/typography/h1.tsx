c4e6cb1ab504c26970f53b4d6cb779920a54c85e
nednella <19449811+nednella@users.noreply.github.com>
2025-12-10 00:24:31 +0000

    Add variable font sizing to h1 and lead

--- apps/web/src/lib/ui/components/typography/h1.tsx
+++ apps/web/src/lib/ui/components/typography/h1.tsx
@@ -6,7 +6,10 @@ export function H1({ ref, className, children }: Readonly<Props>) {
     return (
         <h1
             ref={ref}
-            className={cn("scroll-m-20 text-center text-4xl font-bold tracking-tight text-balance", className)}
+            className={cn(
+                "scroll-m-20 text-center text-4xl font-bold tracking-tight text-balance sm:text-5xl md:text-6xl",
+                className
+            )}
         >
             {children}
         </h1>
