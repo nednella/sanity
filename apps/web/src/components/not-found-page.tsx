d2454efc77745730500af7d79e7cfdd27264f0b6
nednella <19449811+nednella@users.noreply.github.com>
2025-12-10 01:23:30 +0000

    Update NotFound page component to use Hero

---
+++ apps/web/src/components/not-found-page.tsx
@@ -0,0 +1,25 @@
+import { Link } from "@tanstack/react-router"
+
+import { Button } from "@/lib/ui/components/button"
+
+import { Hero } from "./landing/hero"
+
+export function NotFound() {
+    // TODO: add logo
+    return (
+        <div className="flex min-h-screen items-center justify-center overflow-hidden">
+            <Hero
+                title="Page not found"
+                description="we can't seem to find the page you are looking for."
+            >
+                <Button
+                    asChild
+                    size="lg"
+                    className="w-64 rounded-full px-8 sm:w-fit"
+                >
+                    <Link to="/">Go back home</Link>
+                </Button>
+            </Hero>
+        </div>
+    )
+}
