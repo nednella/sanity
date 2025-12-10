30b2accebaa25aa11c93830429ddcbdb36950026
nednella <19449811+nednella@users.noreply.github.com>
2025-12-10 03:18:09 +0000

    Add a ref pass to all ui typography components

--- apps/web/src/lib/ui/components/typography/h3.tsx
+++ apps/web/src/lib/ui/components/typography/h3.tsx
@@ -1,10 +1,14 @@
 import { cn } from "@/lib/ui/utils"
 
-interface Props {
-    className?: string
-    children: React.ReactNode
-}
+type Props = React.ComponentPropsWithRef<"h3">
 
-export function H3({ className, children }: Readonly<Props>) {
-    return <h3 className={cn("scroll-m-20 text-2xl font-semibold tracking-tight", className)}>{children}</h3>
+export function H3({ ref, className, children }: Readonly<Props>) {
+    return (
+        <h3
+            ref={ref}
+            className={cn("scroll-m-20 text-2xl font-semibold tracking-tight", className)}
+        >
+            {children}
+        </h3>
+    )
 }
