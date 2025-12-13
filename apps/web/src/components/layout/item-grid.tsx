4993ba1a537d529a66c1e993fd3872b716b8674b
nednella <19449811+nednella@users.noreply.github.com>
2025-12-13 23:15:32 +0000

    Change ItemGrid cols param to an optional

--- apps/web/src/components/layout/item-grid.tsx
+++ apps/web/src/components/layout/item-grid.tsx
@@ -3,7 +3,7 @@ import type React from "react"
 import { cn } from "@/lib/ui/utils"
 
 type ItemGridProps = React.ComponentPropsWithoutRef<"div"> & {
-    cols: string
+    cols?: string // default grid behaviour is the same as grid-cols-1
 }
 
 export function ItemGrid({ cols, className, children }: Readonly<ItemGridProps>) {
