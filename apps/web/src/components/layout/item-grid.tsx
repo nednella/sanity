8075751d3870767836e81b463bfa2c9ae1e9e3aa
nednella <19449811+nednella@users.noreply.github.com>
2025-12-24 22:59:58 +0000

    Tidy ItemGrid component

--- apps/web/src/components/layout/item-grid.tsx
+++ apps/web/src/components/layout/item-grid.tsx
@@ -2,10 +2,15 @@ import type React from "react"
 
 import { cn } from "@/lib/ui/utils"
 
-type ItemGridProps = React.ComponentPropsWithoutRef<"div"> & {
-    cols?: string // default grid behaviour is the same as grid-cols-1
+type ItemGridProps = React.ComponentPropsWithRef<"div"> & {
+    cols?: string // default grid behaviour is equal to grid-cols-1
 }
 
-export function ItemGrid({ cols, className, children }: Readonly<ItemGridProps>) {
-    return <div className={cn("grid gap-4", cols, className)}>{children}</div>
+export function ItemGrid({ cols, className, ...props }: Readonly<ItemGridProps>) {
+    return (
+        <div
+            className={cn("grid gap-4", cols, className)}
+            {...props}
+        />
+    )
 }
