ba5e36bf87e74f0e801416d15aaf48d724b4386e
nednella <19449811+nednella@users.noreply.github.com>
2025-12-26 20:44:22 +0000

    Re-order nav link config definitions

--- apps/web/src/components/navigation/navigation.config.ts
+++ apps/web/src/components/navigation/navigation.config.ts
@@ -2,36 +2,36 @@ import { linkOptions } from "@tanstack/react-router"
 
 const aboutLinks = linkOptions([
     {
-        title: "About",
         to: "/about",
+        title: "About",
         description: "learn more about who we are"
     },
     {
-        title: "Requirements",
         to: "/requirements",
+        title: "Requirements",
         description: "find out what it takes to join our community"
     },
     {
-        title: "Achievement Diary",
         to: "/diary",
+        title: "Achievement Diary",
         description: "for those looking to push themselves beyond the in-game achievements"
     }
 ])
 
 const memberLinks = linkOptions([
     {
-        title: "Placeholder",
         to: "/",
+        title: "Placeholder",
         description: "Placeholder"
     },
     {
-        title: "Placeholder",
         to: "/",
+        title: "Placeholder",
         description: "Placeholder"
     },
     {
-        title: "Placeholder",
         to: "/",
+        title: "Placeholder",
         description: "Placeholder"
     }
 ])
