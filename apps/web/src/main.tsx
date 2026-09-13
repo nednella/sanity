1e6952fd2e8de21a3e0e075c170cc00ea07374f2
nednella <119449811+nednella@users.noreply.github.com>
2026-09-14 00:20:32 +0100

    feat(web): add daisyui

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./app.js";
+import "./main.css";
 
 createRoot(document.querySelector("#root")!).render(
   <StrictMode>
