7c5c0ff02e7e3d441327462beda5a27dec417ed3
nednella <119449811+nednella@users.noreply.github.com>
2026-09-15 01:59:20 +0100

    feat(web): add theme lib

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,11 +1,17 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 
 import { queryClient } from "@/lib/api/query-client.js";
+import { ThemeProvider } from "@/lib/theme/theme-provider.js";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export function AppProvider({ children }: Readonly<Props>) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ThemeProvider />
+    </QueryClientProvider>
+  );
 }
---
+++ apps/web/src/lib/theme/theme-provider.tsx
@@ -0,0 +1,28 @@
+import { useLayoutEffect } from "react";
+
+import { useThemeStore } from "@/lib/theme/theme.store.js";
+import { applyTheme, onSystemThemeChange } from "@/lib/theme/utils.js";
+
+/**
+ * This provider should be nested within the provider tree next to the application's entry point.
+ */
+export function ThemeProvider() {
+  const { theme } = useThemeStore();
+
+  /**
+   * Applies the new theme to the DOM as soon as it's updated,
+   * and if set to system, appends an event listener for OS theme
+   * preference changes.
+   *
+   * The listener is removed on any re-render when the deps change.
+   */
+  useLayoutEffect(() => {
+    applyTheme(theme);
+
+    if (theme !== "system") return;
+    const unsubscribe = onSystemThemeChange(() => applyTheme("system"));
+    return unsubscribe;
+  }, [theme]);
+
+  return null;
+}
