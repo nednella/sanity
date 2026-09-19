83224250f89b3e62c852fc790710d74a6caec999
nednella <119449811+nednella@users.noreply.github.com>
2026-09-19 19:53:37 +0100

    feat(osrs): sort metrics into their in-game order

--- packages/osrs/src/index.ts
+++ packages/osrs/src/index.ts
@@ -1,2 +1,2 @@
 export { Activity, Boss, Computed, METRICS, Skill } from "./metrics/enum.js";
-export { metricName } from "./metrics/utils.js";
+export { metricName, sortActivities, sortBosses, sortSkills } from "./metrics/utils.js";
