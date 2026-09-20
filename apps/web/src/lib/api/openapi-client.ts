e1a2d3992a2fff17ad924a86aa64d5726616df6f
nednella <119449811+nednella@users.noreply.github.com>
2026-09-20 02:46:04 +0100

    feat(web): add a config import alias

--- apps/web/src/lib/api/openapi-client.ts
+++ apps/web/src/lib/api/openapi-client.ts
@@ -3,9 +3,9 @@ import createQueryClient from "openapi-react-query";
 
 import type { paths } from "@sanity/api/v1";
 
-import { errorMiddleware } from "@/lib/api/middleware";
+import { config } from "@config";
 
-import { config } from "../../../config";
+import { errorMiddleware } from "@/lib/api/middleware";
 
 const createOpenApiClient = () => {
   const client = createFetchClient<paths>({ baseUrl: config.apiUrl });
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
     tailwindcss()
   ],
   resolve: {
-    alias: { "@": new URL("src", import.meta.url).pathname }
+    alias: {
+      "@config": new URL("config.ts", import.meta.url).pathname,
+      "@": new URL("src", import.meta.url).pathname
+    }
   }
 });
